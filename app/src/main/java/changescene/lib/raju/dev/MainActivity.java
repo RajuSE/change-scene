40d4a4d5ea93c9c88e1c7b3deeef6a1dfdfd63c5
Raju <git.dev.raju@gmail.com>
2017-06-09 14:04:15 +0530

    Initial Project

---
+++ app/src/main/java/changescene/lib/raju/dev/MainActivity.java
@@ -0,0 +1,13 @@
+package changescene.lib.raju.dev;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(changescene.lib.raju.dev.change_scene.R.layout.activity_main);
+    }
+}
